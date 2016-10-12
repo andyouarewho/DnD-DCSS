b87cfcfdee57be4085d234bf1716e702e2d6e3e9
andyouarewho <j.norm.mann@gmail.com>
2016-10-11 21:13:28 -0400

    Add files via upload

--- chargen.py
+++ chargen.py
@@ -1,69 +1,101 @@
+"""Character Generator"""
 import random
 
-
-def human(player):
-    player.strength += 6
-    player.intelligence += 6
-    player.dexterity += 6
-    player.race_name = 'human'
-def kobold(player):
-    player.strength += 5
-    player.intelligence += 4
-    player.dexterity += 8
-    player.race_name = 'kobold'
-def dwarf(player):
-    player.strength += 9
-    player.intelligence += 6
-    player.dexterity += 6
-    player.race_name = 'dwarf'
-def deep_elf(player):
-    player.strength += 3
-    player.intelligence += 10
-    player.dexterity += 8
-    player.race_name = 'deep elf'
-def orc(player):
-    player.strength += 8
-    player.intelligence += 6
-    player.dexterity += 8
-    player.race_name = 'orc'
+class Character():
+    def __init__(self, race = None, profession = None, strength=0, intelligence=0, dexterity=0):
+        self.race = race
+        self.profession = profession
+        self.strength = strength
+        self.intelligence = intelligence
+        self.dexterity = dexterity
+    def __repr__(self):
+    	return '{} {}'.format(self.race, self.profession)
 
 
-def fighter(player):
-    player.strength += 8
-    player.dexterity += 4
-    player.profession_name = 'fighter'
-def wizard(player):
-    player.strength -= 1
-    player.intelligence += 10
-    player.dexterity += 3
-    player.profession_name = 'wizard'
+class Race():
+    def __init__(self, racename='Unknown', strmod=0, intmod=0, dexmod=0):
+        self.racename = racename
+        self.strmod = strmod
+        self.intmod = intmod
+        self.dexmod = dexmod
+    def __repr__(self):
+    	return self.racename
 
 
-class NewPlayer(object):
-    def __init__(self):
-        self.strength = 0
-        self.intelligence = 0
-        self.dexterity = 0
-        self.race_name = None
-        self.race = None
-        self.profession_name = None
-        self.profession = None
-        racelist = [
-            human, kobold, dwarf, deep_elf, orc
-            ]
-        proflist = [
-            fighter, wizard
-        ]
-        blah = random.randint(0, len(racelist)-1)
-        blarg = random.randint(0, 1)
-        plop = racelist[blah]
-        plop(self)
-        plank = proflist[blarg]
-        plank(self)
+class Profession():
+    def __init__(self, profname='Unknown', strmod=0, intmod=0, dexmod=0):
+        self.profname = profname
+        self.strmod = strmod
+        self.intmod = intmod
+        self.dexmod = dexmod
+    def __repr__(self):
+    	return self.profname
+
+def random_roll_race():
+    r = {
+        1 : 'human',
+        2 : 'kobold',
+        3 : 'dwarf',
+        4 : 'elf',
+        5 : 'orc'
+        }
+    x = r[random.randint(1, 5)]
+    return x
+def random_roll_prof():
+    r = {
+        1 : 'fighter',
+        2 : 'wizard'
+        }
+    y = r[random.randint(1, 2)]
+    return y
+
+def stat_fix(bob): # SLOPPY WAY TO FIX LOW STAT CONDITION
+    if bob.strength < 3:
+        roll = random.randint(1, 100)
+        if roll < 50:
+            bob.strength += 1
+            bob.dexterity -= 1
+        else:
+            bob.strength += 1
+            bob.intelligence -= 1
+    elif bob.intelligence < 3:
+        roll = random.randint(1, 100)
+        if roll < 50:
+            bob.intelligence += 1
+            bob.dexterity -= 1
+        else:
+            bob.intelligence += 1
+            bob.strength -= 1
+    elif bob.dexterity < 3:
+        roll = random.randint(1, 100)
+        if roll < 50:
+            bob.dexterity += 1
+            bob.intelligence -= 1
+        else:
+            bob.dexterity += 1
+            bob.strength -= 1
+    else:
+        None
+
+RACES = {
+    'human' : Race(racename='human', strmod=6, intmod=6, dexmod=6),
+    'kobold' : Race(racename='kobold', strmod=5, intmod=4, dexmod=8),
+    'dwarf' : Race(racename='dwarf', strmod=9, intmod=6, dexmod=6),
+    'elf' : Race(racename='elf', strmod=3, intmod=10, dexmod=8),
+    'orc' : Race(racename='orc', strmod=8, intmod=6, dexmod=4)
+    }
 
 
-bob = NewPlayer()
+PROFESSIONS = {
+    'fighter' : Profession(profname='fighter', strmod=8, intmod=0, dexmod=4),
+    'wizard' : Profession(profname='wizard', strmod=-1, intmod=10, dexmod=3)
+    }
 
 
-print bob.strength, bob.intelligence, bob.dexterity,
-print bob.race_name, bob.profession_name
+john = Character(RACES[random_roll_race()], PROFESSIONS[random_roll_prof()])
+john.strength += john.race.strmod + john.profession.strmod
+john.intelligence += john.race.intmod + john.profession.intmod
+john.dexterity += john.race.dexmod + john.profession.dexmod
+stat_fix(john)
+print str.upper(john.race.racename), str.upper(john.profession.profname)
+print 'STR:', john.strength, 'INT:', john.intelligence, 'DEX:', john.dexterity
